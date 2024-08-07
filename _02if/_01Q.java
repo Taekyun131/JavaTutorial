6efcf6da48065a80755ed6cb4c32425fac90f362
Taekyun <stk131@naver.com>
2024-08-07 11:53:12 +0900

    if

---
+++ _02if/_01Q.java
@@ -0,0 +1,25 @@
+package _02if;
+
+public class _01Q {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*수강관리 프로젝트를 진행한다.
+		학생의 정보는 이름 학번 나이 성적이다.
+		나이가 18세 미만은 수강신청을 할 수 없다.
+		성적이 60점 이상이면서 70점 미만인 학생은 수강신청을 할 수 있지만, 이름 뒤에 별표를 추가한다.
+		학생의 성적을 기준으로 
+		90점 이상 A
+		80점 이상 B
+		70점 이상 C
+		60점 이상 D
+		60점 미만 F 출력*/
+		
+		String name;
+		int num;
+		int age;
+		int score;
+		
+	}
+
+}
