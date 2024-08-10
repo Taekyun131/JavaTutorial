c8497232ce9916e426cdbf7a4f9f3f5879a1d53e
Taekyun <stk131@naver.com>
2024-08-10 21:57:13 +0900

    for_mission2_문제풀이

--- _03for/_25_mission2_15.java
+++ _03for/_25_mission2_15.java
@@ -4,6 +4,25 @@ public class _25_mission2_15 {
 	public static void main(String[] args) {
 		//문자열 압축하기 압축 하는 방법 a2b3c3a4d2b3a5
 		String ttt="aabbbcccaaaaddbbbaaaaa";
-		
+		char a='a';
+		int count=1;
+		String c=" ";
+		for(int i=1;i<22;i++) {
+			if(ttt.charAt(i)==a) {
+				count++;
+			}else {
+				c+=a;
+				c+=count;
+				count=1;
+			}
+			if(i==21) {
+				c+=a;
+				c+=count;
+				
+			}
+			a=ttt.charAt(i);
+			
+		}
+		System.out.println(c);
 	}
 }
