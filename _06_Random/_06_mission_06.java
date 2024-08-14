2df233cee42c2487d9f5ffe9ce5be1906a20c1af
Taekyun <stk131@naver.com>
2024-08-14 12:51:07 +0900

    Random_문제풀이

---
+++ _06_Random/_06_mission_06.java
@@ -0,0 +1,19 @@
+package _06_Random;
+
+import java.util.Random;
+
+public class _06_mission_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*컴퓨터는 랜덤으로 로또 번호를 10000번 뽑는다.
+		가장 많이 뽑힌 로또 번호가 오늘의 추천 로또 번호이다.
+		오늘의 추천 로또 번호 하나를 출력하시오.  
+		만약 로또 번호가 같은 횟수로 나왔을 경우에는 큰 숫자가 우선한다. */
+		for(int i=0;i<10000;i++) {
+			Random r=new Random();
+			int random=r.nextInt(44)+1;
+		}
+	}
+
+}
