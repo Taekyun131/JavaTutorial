b3173168314eba1ac82e8137f950f7ff76fbf0f3
Taekyun <stk131@naver.com>
2024-08-19 17:30:17 +0900

    2중for_문제풀이

--- _07_2for/_02_mission_02.java
+++ _07_2for/_02_mission_02.java
@@ -9,6 +9,7 @@ public class _02_mission_02 {
 	//		***
 	//		**
 	//		*
+		
 		for(int i=0;i<=5;i++) {
 			for(int j=0;j<5-i;j++) {
 				System.out.print("*");
--- _07_2for/_05_mission_05.java
+++ _07_2for/_05_mission_05.java
@@ -5,6 +5,7 @@ public class _05_mission_05 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//40보다 큰 수는 모두 몇개인가?
+		
 		int count=0;
 		int[] a ={34,55,23,56,34,45,34};
 		for(int i=0;i<a.length;i++) {
