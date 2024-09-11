e55769d9659c491cce9cbcb7c799c7f011a0aca8
TaekyunShin <stk131@naver.con>
2024-09-11 11:06:54 +0900

    java_2차원배열_mission_분석

--- _10_Array2/_07_mission_07.java
+++ _10_Array2/_07_mission_07.java
@@ -34,15 +34,15 @@ public class _07_mission_07 {
 				temp=game[i][j];
 //				System.out.println("a: "+game[i][j]);
 //				System.out.println(count);
-			if(count>=3) {
-//				System.out.println(i+","+j);
-//				System.out.println(count);
+				if(count>=3) {
+//					System.out.println(i+","+j);
+//					System.out.println(count);
 //				
-				for(int k=0;k<count;k++) {
-					System.out.println("("+(i-(count-k-1))+","+j+")");
+					for(int k=0;k<count;k++) {
+						System.out.println("("+(i-(count-k-1))+","+j+")");
+					}
+					System.out.println();
 				}
-				System.out.println();
-			}
 			}
 		
 		
@@ -50,3 +50,18 @@ public class _07_mission_07 {
 	}
 
 }
+
+
+/* 분석
+ * 세로로 같은 숫자 연속 3개 이상이면 블록의 좌표 출력
+ * 2차원배열을 행렬로 생각
+ * i값을 2차원배열의 행, j값을 열이라하면
+ * j값을 0부터 4까지 변화시킬 때, i값을 0부터 4까지 변화
+ * 같은 숫자인 개수를 세어줄 변수 선언하고, 열이 바뀔 때마다 0으로 초기화
+ * 배열값과 같은지 비교할 변수 선언하고, 비교 후 변수는 해당 배열값으로 치환
+ * 변수와 배열값이 같은지 비교할 조건문 작성
+ * 같으면 개수를 세어주는 변수 증가, 다르면 변수를 1로 초기화
+ * 개수가 3이상이면 제거가능한 좌표를 출력
+ * 개수마다 출력할 값 보정
+ * 
+ */
