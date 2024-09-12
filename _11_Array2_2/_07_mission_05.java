c4c4e36de11f776cc3e6f477500d2ee041d7daa8
TaekyunShin <stk131@naver.con>
2024-09-12 12:51:13 +0900

    java_2차배열2_mission_추가

--- _11_Array2_2/_07_mission_05.java
+++ _11_Array2_2/_07_mission_05.java
@@ -14,27 +14,83 @@ public class _07_mission_05 {
 		int num=1;
 		
 		//풀이
-		for(int j=0;j<a[0].length;j++) {
-			a[0][j]=num;
-			num++;
-		}
-		for(int i=1;i<a.length;i++) {
-			a[i][4]=num;
-			num++;
-		}
-		for(int j=1;j<a[0].length;j++) {
-			a[3][a[0].length-j-1]=num;
-			num++;
+//		for(int j=0;j<a[0].length;j++) {
+//			a[0][j]=num;
+//			num++;
+//		}
+//		for(int i=1;i<a.length;i++) {
+//			a[i][4]=num;
+//			num++;
+//		}
+//		for(int j=1;j<a[0].length;j++) {
+//			a[3][a[0].length-j-1]=num;
+//			num++;
+//		}
+//		
+//		for(int i=1;i<3;i++) {
+//			a[a.length-i-1][0]=num;
+//			num++;
+//		}
+//			
+//		for (int j=0;j<a[0].length;j++) {
+//			a[0][j]=num;
+//			num++;
+//		}
+//		for(int i=0;i<a.length-1;i++) {
+//			a[i+1][4]=num;
+//			num++;
+//		}
+			
+		//가로
+//		for(int k=0;k<4;k++) {
+//			if(k%2==0) {
+//				for(int j=0;j<a[0].length-k;j++) {
+//					a[k/2][j+k/2]=num;
+//					num++;
+//				}
+//			}else {
+//				for(int j=0;j<a[0].length-k;j++) {
+//					a[(7-k)/2][a[0].length-j-(k+3)/2]=num;
+//					num++;
+//					}
+//				}
+//			}
+		
+		for(int k=0;k<7;k++) {
+			if(k%4==0) {
+				for(int j=0;j<a[0].length-k/2;j++) {
+					a[k/4][j+k/4]=num;
+					num++;
+				}
+			}
+			else if(k%4==1) {
+				for(int i=0;i<a.length-(k+1)/2;i++) {
+					a[(k+3)/4+i][(17-k)/4]=num;
+					num++;
+				}
+			}
+			else if(k%4==2) {
+				for(int j=0;j<a[0].length-k/2;j++) {
+					a[(14-k)/4][a[0].length-j-(k+6)/4]=num;
+					num++;
+				}
+			}
+			else {
+				for(int i=0;i<a.length-(k+5)/4;i++) {
+					a[a.length-i-(k+5)/4][0]=num;
+					num++;
+				}
+			}
 		}
 		
-		for(int i=1;i<3;i++) {
-			a[a.length-i-1][0]=num;
-			num++;
-		}
 			
+		
+		
+		
 			
+		
 			
-			
+		
 			
 		
 		
@@ -42,7 +98,7 @@ public class _07_mission_05 {
 		//출력
 		for(int i=0;i<a.length;i++) {
 			for(int j=0;j<a[0].length;j++) {
-				System.out.print(a[i][j]+" ");
+				System.out.print(a[i][j]+"\t ");
 			}
 			System.out.println();
 		}
---
+++ _11_Array2_2/_08_mission_06.java
@@ -0,0 +1,42 @@
+package _11_Array2_2;
+
+public class _08_mission_06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//6.	1	2	3	4	5		6	1	2	3	4
+		//		6	7	8	9	10		11	12	7	8	5
+		//		11	12	13	14	15	>>	16	17	13	9	10
+		//		16	17	18	19	20		21	18	19	14	15
+		//		21	22	23	24	25		22	23	24	25	20
+		
+		//배열값 입력부
+		int [][]a= {{1,2,3,4,5},
+				{6,7,8,9,10},
+				{11,12,13,14,15},
+				{16,17,18,19,20},
+				{21,22,23,24,25}
+				};
+		
+		//풀이
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		//출력
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a[0].length;j++) {
+				System.out.print(a[i][j]+"\t ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
