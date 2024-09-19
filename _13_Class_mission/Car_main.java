c8a1c6c195684dc72d2f2e23b6e0a9b21984b23b
TaekyunShin <stk131@naver.con>
2024-09-19 17:26:45 +0900

    java_Class_mission

---
+++ _13_Class_mission/Car_main.java
@@ -0,0 +1,11 @@
+package _13_Class_mission;
+
+public class Car_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("주차등록 프로그램");
+		Car_m carMg=new Car_m();
+	}
+
+}
