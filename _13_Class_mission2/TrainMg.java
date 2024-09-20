37c3d67ea6860f0a0aa71f3274fc29064fede17c
TaekyunShin <stk131@naver.con>
2024-09-20 16:59:29 +0900

    java_class_mission2

--- _13_Class_mission2/TrainMg.java
+++ _13_Class_mission2/TrainMg.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class TrainMg {
 	TrainReg [] trainList=new TrainReg[5];
+	int length=trainList.length;
 	Scanner in=new Scanner(System.in);
 	TrainMg(){
 		while(true) {
 			menu();
 			int selNum=in.nextInt();
+			in.nextLine();
 			if(selNum==1) {
 				System.out.println("1. 기차시간 등록하기를 선택");
 				addTrain();
@@ -16,38 +18,151 @@ public class TrainMg {
 				System.out.println("2. 기차시간 전체보기를 선택");
 				allTrain();
 			}else if(selNum==3) {
-				System.out.println("3. 기차시간 수정하기를 선");
+				System.out.println("3. 기차시간 수정하기를 선택");
 				modTrain();
 			}else if(selNum==4) {
-				
+				System.out.println("4. 기차 수리정보 보기를 선택");
+				fixTrain();
 			}else {
-				break;
+				continue;
 			}
 		}
 	}
+	//0. 메뉴
 	public void menu() {
 		System.out.println("*****번호를 선택하세요*****");
 		System.out.println("1. 기차시간 등록하기");
 		System.out.println("2. 기차시간 전체보기");
 		System.out.println("3. 기차시간 수정하기");
+		System.out.println("4. 기차 수리정보보기");
 		System.out.println(">>>");
 	}
+	
+	//1. 등록하기
 	public void addTrain() {
-		
+		TrainReg train=new TrainReg();
 		System.out.println("기차번호를 입력하세요");
-		String newTrainNum=in.nextLine();
-		System.out.println("기차시간을 입력하세요");
-		String newTrainTime=in.nextLine();
-		for(int i=0;i<trainList.length;i++) {
-			if(trainList[i]==null) {
-				
+		train.TrainNum=in.nextInt();
+		in.nextLine();
+//		System.out.println("기차시간을 입력하세요");
+//		train.TrainTime=in.nextLine();
+//		for(int i=0;i<trainList.length;i++) {
+//			if(trainList[i]==null) {
+//				trainList[i]=train;
+//				break;
+//			}
+//		}
+		
+		
+		if(train.TrainNum==1111||train.TrainNum==3333) {
+			train.TrainType="무궁화";
+			System.out.println("도착시간을 입력하세요");
+			train.TrainTime=in.nextLine();
+			for(int i=0;i<length;i++) {
+				if(trainList[i]==null) {
+					trainList[i]=train;
+					break;
+				}
+			}
+		}else if(train.TrainNum==2222||train.TrainNum==4444) {
+			train.TrainType="새마을";
+			System.out.println("도착시간을 입력하세요");
+			train.TrainTime=in.nextLine();
+			for(int i=0;i<length;i++) {
+				if(trainList[i]==null) {
+					trainList[i]=train;
+					break;
+				}
+			}
+		}else {
+			System.out.println("해당 기차가 없습니다.");
+		}
+		
+//		System.out.println("도착시간을 입력하세요");
+//		train.TrainTime=in.nextLine();
+//		in.nextLine();
+//		for(int i=0;i<trainList.length;i++) {
+//			if(trainList[i]==null) {
+//				trainList[i]=train;
+//				break;
+//			}
+//		}
+	}
+	
+	
+	//2. 전체보기
+	public void allTrain() {
+		for(int i=0;i<length;i++) {
+			if(trainList[i]!=null) {
+				trainList[i].showTrain();
 			}
 		}
 	}
-	public void allTrain() {
-		
-	}
+	
+	
+	//3. 수정하기
 	public void modTrain() {
-		
+		System.out.println("수정할 기차번호를 입력하세요");
+		int modTrainNumber=in.nextInt();
+		in.nextLine();
+//		for(int i=0;i<trainList.length;i++) {
+//			if(trainList[i]!=null) {
+//				if(trainList[i].TrainNum==modTrainNumber) {
+//					String modTrainTime=in.nextLine();
+//					System.out.println("기차의 수정된 시간을 입력학세요");
+//					trainList[i].TrainTime=modTrainTime;
+//					break;
+//				}
+//			}
+//		}
+		for(int i=0;i<length;i++) {
+			if(trainList[i]!=null) {
+				if(trainList[i].TrainNum==modTrainNumber) {
+					System.out.println("기존 도착시간을 입력하세요");
+					String TrainTime=in.nextLine();
+					if(trainList[i].TrainTime.equals(TrainTime)) {
+						System.out.println("수정된 도착시간을 입력하세요");
+						String modTrainTime=in.nextLine();
+						trainList[i].TrainTime=modTrainTime;
+						break;
+					}
+				}
+			}
+		}
+	}
+	//수리중기차메뉴
+	public void fixTrain() {
+		while(true) {
+			System.out.println("-----번호를 선택하세요-----");
+			System.out.println("1. 기차 수리등록");
+			System.out.println("2. 수리중인 기차보기");
+			System.out.println("3. 나가기");
+			int selNum=in.nextInt();
+			in.nextLine();
+			if(selNum==1) {
+				System.out.println("수리할 기차번호를 등록하세요");
+				int fixTrainNum=in.nextInt();
+				if(fixTrainNum%1111==0) {
+					for(int i=0;i<length;i++) {
+						if(trainList[i]!=null) {
+							if(trainList[i].TrainNum==fixTrainNum){
+								trainList[i].TrainState="수리중";
+										break;
+							}
+						}
+					}
+				}else {
+					System.out.println("기차번호를 다시 입력하세요");
+				}
+			}else if(selNum==2) {
+				for(int i=0;i<length;i++) {
+					if(trainList[i]!=null) {
+						trainList[i].showFixTrain();
+					}
+				}
+			}else {
+				break;
+			}
+		}
 	}
 }
--- _13_Class_mission2/TrainReg.java
+++ _13_Class_mission2/TrainReg.java
@@ -1,13 +1,71 @@
 package _13_Class_mission2;
 
 public class TrainReg {
-	String TrainNum=null;
+	int TrainNum=0;
 	String TrainTime=null;
+	String TrainType=null;
+	String TrainState=null;
 	TrainReg(){
 		
 	}
+	//일반기차 전체보기
 	public void showTrain() {
-		System.out.println(TrainNum);
-		System.out.println(TrainTime);
+		if(TrainState!="수리중") {
+			//기차이름
+			int trainName=TrainNum/1111;
+			System.out.println(trainName+"번열차");
+//		if(TrainNum==1111) {
+//			System.out.println("기차이름: 1번열차");
+//		}else if(TrainNum==2222) {
+//			System.out.println("기차이름: 2번열차");
+//		}else if(TrainNum==3333) {
+//			System.out.println("기차이름: 3번열차");
+//		}else if(TrainNum==4444) {
+//			System.out.println("기차이름: 4번열차");
+//		}
+			
+			//기차번호
+			System.out.println("기차번호: "+TrainNum);
+//		System.out.println("기차종류: "+TrainType);
+			
+			//기차종류
+			if(TrainType=="새마을") {
+				System.out.println("기차종류: "+TrainType+"*");
+			}else {
+				System.out.println("기차종류: "+TrainType);
+			}
+			
+			//도착시간
+			System.out.println("도착시간: "+TrainTime);
+			System.out.println();
+		}
 	}
+	
+	
+	
+	//수리중 기차보기
+	public void showFixTrain() {
+		if(TrainState=="수리중") {
+			
+			//기차이름
+			int trainName=TrainNum/1111;
+			System.out.println(trainName+"번열차");
+			
+			//기차번호
+			System.out.println("기차번호: "+TrainNum);
+			
+			
+			//기차종류
+			if(TrainType=="새마을") {
+				System.out.println("기차종류: "+TrainType+"*");
+			}else {
+				System.out.println("기차종류: "+TrainType);
+			}
+			
+			//도착시간
+			System.out.println("도착시간: "+TrainTime);
+			System.out.println();
+		}
+		}
+	
 }
